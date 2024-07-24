a770b42c799a391c48466dee8b89f1e252286390
Zwiicker <hickzwicker@gmail.com>
2024-07-23 21:59:20 -0300

    feat: add 'Skills' section with skill bars and icons.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { FaReact, FaJsSquare,FaHtml5,FaAngular,FaJava,FaPython} from 'react-icons/fa'; // Usando react-icons
+import { SiTypescript,SiCsharp,SiDotnet} from 'react-icons/si';
 import profilePic from '../assets/profile-pic.jpg'; // Substitua pelo caminho da sua imagem
 
+
+
 function Home() {
+
+  const skills = [
+    { name: "HTML & CSS",  icon: <FaHtml5 className="text-orange-600 text-2xl" /> ,level: 60 },
+    { name: "JavaScript",  icon: <FaJsSquare className="text-yellow-500 text-2xl" /> ,level: 50 },
+    { name: "C#", icon: <SiCsharp className="text-purple-600 text-2xl" />, level: 50  },
+    { name: "Java", icon: <FaJava className="text-red-700 text-2xl" />, level: 50  },
+    { name: "React", icon: <FaReact className="text-blue-600 text-2xl" /> ,level: 35 },
+    { name: "Angular", icon: <FaAngular className="text-red-600 text-2xl" />, level: 30 },
+    { name: "TypeScript", icon: <SiTypescript className="text-blue-500 text-2xl" />,level: 30 },
+    { name: ".NET", icon: <SiDotnet className="text-purple-700 text-2xl" />, level: 30  },
+    { name: "Python", icon: <FaPython className="text-blue-600 text-2xl" />,level: 30  },
+
+  ];
 
   return (
     <div className="flex flex-col min-h-screen font-poppins">
@@ -22,7 +39,35 @@ function Home() {
               <p className="text-2xl   text-blue-950">Desenvolvedor focado em carreira na programação, sempre buscando aprender e crescer em áreas como desenvolvimento web e frameworks.</p>
             </div>
           </div>
-        </section>                   
+        </section>
+
+     {/* Seção Habilidades */}
+      <section id="habilidades" className="mb-20">
+        {/* Grade responsiva para exibir as habilidades */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {/* Mapeamento das habilidades para renderização */}
+          {skills.map(skill => (
+            <div key={skill.name} className="p-4 border border-gray-300 rounded-lg bg-white shadow-md animate-grow">
+              <div className="flex items-center space-x-4 mb-4">
+                <div className="w-12 h-12 flex items-center justify-center rounded-full bg-gray-200">
+                  {skill.icon}
+                </div>
+                <h3 className="text-xl font-semibold">{skill.name}</h3>
+              </div>
+              <div className="relative pt-1">
+                <div className="flex items-center justify-between mb-2">
+                </div>
+                <div className="flex h-4 mt-1 overflow-hidden rounded-full bg-gray-200">
+                  <div
+                    className="flex flex-col justify-center text-center whitespace-nowrap bg-gradient-to-r from-blue-400 bg-blue-950 animate-grow"
+                    style={{ width: `${skill.level}%` }}
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>       
+      </section>        
       </main>
     </div>
   );
